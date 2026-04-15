eea355787c90db57724d27b4175f9ce247e426bd
PC_do_Batista <gabriel.code22@gmail.com>
2026-04-14 23:25:44 -0300

    Adicionado o pdf do projeto

--- input/entrada.pas
+++ input/entrada.pas
@@ -1,17 +1,13 @@
-program TesteCompleto;
+program SistemaCalculo;
 var
 a, b, c : integer;
-x : real;
-
+x, y : real;
 begin
+{ inicialização }
 a := 10;
 b := 20;
-c := a + b * 2;
-
-x := 3.14;
-x := 20.;        { erro número mal formado }
-
-y := 10 @ 2;     { erro caractere inválido }
-
-{ comentário não fechado
+c := a + b * 3;
+{ operações com real }
+x := 2.5;
+y := x * 10.0 + c;
 end.
